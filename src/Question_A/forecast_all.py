b30c86dd13858db81c495557a425e2cdc37329c3
Konstantinos <ktheofilis@gmail.com>
2022-01-06 18:48:52 +0200

    Fixes

--- src/Question_A/forecast_all.py
+++ src/Question_A/forecast_all.py
@@ -1,7 +1,10 @@
 import os
 import matplotlib.pyplot as plt
+from numpy.core.multiarray import empty
 import pandas as pd
 import numpy as np
+import sys
+import string
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
@@ -9,28 +12,48 @@ from keras.layers import Dropout
 from keras.layers import *
 from sklearn.preprocessing import MinMaxScaler
 
-curr_dir = os.path.abspath(__file__)
-csv_path = "..\..\dir\\nasd_input.csv"
-model_path = "..\..\Forecast_model.h5"
+if len(sys.argv) < 5:
+    print("Too few arguments")
+    quit()
+elif len(sys.argv) > 5:
+    print("Too many arguments")
+    quit()
 
-df = pd.read_csv(os.path.join(curr_dir, csv_path), header=None, delimiter='\t')
+for i in range (1, len(sys.argv)):
+    if(sys.argv[i] == "-d"):
+        csv_path = sys.argv[i+1]
+    elif(sys.argv[i] == "-n"):
+        num = int(sys.argv[i+1])
+
+
+curr_dir = os.path.abspath(__file__)
+model_path = "../../Forecast_model.h5"
+testpath = "/home/theo/Desktop/LSTM_project/dir/nasd_input.csv"
+
+#1. append all -> train -> save image
+
+df = pd.read_csv(testpath, header=None, delimiter='\t')
+file_ids = df.iloc[:, [0]].values
 df = df.drop(df.columns[0], axis=1)
 df = df.transpose()
 print("Number of rows and columns:", df.shape)
 
-training_set = df.iloc[:600, [1]].values  # Training set: first 800 rows
-
-sc = MinMaxScaler(feature_range = (0, 1))
-training_set_scaled = sc.fit_transform(training_set)
-
+sc = MinMaxScaler(feature_range = (0, 1))    
 # Creating a data structure with 60 time-steps and 1 output
 X_train = []
 y_train = []
-for i in range(60, 600):
-    X_train.append(training_set_scaled[i-60:i, 0])
-    y_train.append(training_set_scaled[i, 0])
+for step in range (0 , num):
+    
+    training_set = df.iloc[:600, [step]].values  # Training set: first 800 rows
+    training_set_scaled = sc.fit_transform(training_set)
+
+    for i in range(60, 600):
+        X_train.append(training_set_scaled[i-60:i, 0])
+        y_train.append(training_set_scaled[i, 0])
+
 X_train, y_train = np.array(X_train), np.array(y_train)
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
 # Shape of X_train: (740, 60, 1)
 
 model = Sequential()
@@ -76,14 +99,19 @@ print(X_test.shape)
 # (459, 60, 1)
 
 predicted_stock_price = model.predict(X_test)
+print(len(predicted_stock_price))
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
+print(len(dataset_test.values))
+print(len(predicted_stock_price))
+
 # Visualising the results
-plt.plot(df.loc[800:, 'Date'],dataset_test.values, color = 'red', label = 'Real TESLA Stock Price')
-plt.plot(df.loc[800:, 'Date'],predicted_stock_price, color = 'blue', label = 'Predicted TESLA Stock Price')
-plt.xticks(np.arange(0,459,50))
+plt.plot(dataset_test.values, color = 'red', label = 'Real TESLA Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted TESLA Stock Price')
+# plt.xticks(np.arange(0,459,50))
 plt.title('TESLA Stock Price Prediction')
 plt.xlabel('Time')
 plt.ylabel('TESLA Stock Price')
 plt.legend()
 plt.show()
+plt.savefig('q1a1.png')
